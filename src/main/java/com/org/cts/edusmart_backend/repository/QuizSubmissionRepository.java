07344b7330c63fa5edf3930caaa121ff0cc12d5b
Mittal <Saurabh.Mittal2@cognizant.com>
2026-02-26 14:53:03 +0530

    little bit of changes

--- src/main/java/com/org/cts/edusmart_backend/repository/QuizSubmissionRepository.java
+++ src/main/java/com/org/cts/edusmart_backend/repository/QuizSubmissionRepository.java
@@ -6,5 +6,7 @@ import java.util.List;
 
 public interface QuizSubmissionRepository extends JpaRepository<QuizSubmission, Long> {
     List<QuizSubmission> findByQuizId(Long quizId);
+    List<QuizSubmission> findByStudentId(Long studentId);
+
 
 }
